07cec2ce49d64783ccecd89b00c7f14f885f3012
thatiah <thatiahnia@gmail.com>
2026-07-24 11:28:01 +0300

    drop user model from the bookstore app, create accounts app and add user model to make code cleaner

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BookStoreApp',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
