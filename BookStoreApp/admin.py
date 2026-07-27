22d11b3ae8ecd7fb3841c81980ed85a4add2e819
thatiah <thatiahnia@gmail.com>
2026-07-27 09:22:14 +0300

    register models in admin.py

--- BookStoreApp/admin.py
+++ BookStoreApp/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Category , Book
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Book)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(Favorite)
+admin.site.register(StockNotification)
