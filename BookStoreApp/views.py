9dfb3cc32bf54dd98ac0e5e750442aa29f1a3662
thatiah <thatiahnia@gmail.com>
2026-07-27 09:59:27 +0300

    create viewsets for book and category

--- BookStoreApp/views.py
+++ BookStoreApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Book, Category
+from .serializers import BookSerializer, CategorySerializer
 
 # Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
