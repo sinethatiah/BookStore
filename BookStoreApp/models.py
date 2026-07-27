b23743150e317b3bfcd40feab2cda6942ef3c6ae
thatiah <thatiahnia@gmail.com>
2026-07-27 09:01:04 +0300

    add model for cart items

--- BookStoreApp/models.py
+++ BookStoreApp/models.py
@@ -33,6 +33,14 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"Cart ({self.user})"
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.book.title}"
 
 class Order(models.Model):
     DELIVERY_METHOD = [('pickup', 'Pickup'), ('delivery', 'Delivery')]
