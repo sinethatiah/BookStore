d5f36ab82a70399b9799f432eba90036efcb7179
thatiah <thatiahnia@gmail.com>
2026-07-28 06:22:10 +0300

    register urls for the cart items and cart viewsets

--- BookStoreApp/urls.py
+++ BookStoreApp/urls.py
@@ -1,10 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, CategoryViewSet,OrderViewSet, OrderItemViewSet
+from .views import BookViewSet, CategoryViewSet,OrderViewSet, OrderItemViewSet ,CartViewSet, CartItemViewSet
 
 router = DefaultRouter()
 router.register('books', BookViewSet)
 router.register('categories', CategoryViewSet)
 router.register('orders', OrderViewSet)
 router.register('order-items', OrderItemViewSet)
+router.register('cart', CartViewSet)
+router.register('cart-items', CartItemViewSet)
 
 urlpatterns = router.urls
