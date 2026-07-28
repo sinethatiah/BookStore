d3f83db0ef0846b69a5d74cd868ded462ac9d9c8
thatiah <thatiahnia@gmail.com>
2026-07-28 06:02:58 +0300

    register urls for order and orderitems apis

--- BookStoreApp/urls.py
+++ BookStoreApp/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, CategoryViewSet
+from .views import BookViewSet, CategoryViewSet,OrderViewSet, OrderItemViewSet
 
 router = DefaultRouter()
 router.register('books', BookViewSet)
 router.register('categories', CategoryViewSet)
+router.register('orders', OrderViewSet)
+router.register('order-items', OrderItemViewSet)
 
 urlpatterns = router.urls
