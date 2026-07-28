d8726407a6c08f56af235c86cfeb9f8ab99c8589
thatiah <thatiahnia@gmail.com>
2026-07-28 05:48:50 +0300

    make serializers for order models and order item model

--- BookStoreApp/serializers.py
+++ BookStoreApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Book
+from .models import Category, Book , Order, OrderItem
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,4 +8,16 @@ class CategorySerializer(serializers.ModelSerializer):
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
+        fields = '__all__'
+        
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
         fields = '__all__'
