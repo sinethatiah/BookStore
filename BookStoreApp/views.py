96690b3902b9d679e72b14535f9263834f19fc81
thatiah <thatiahnia@gmail.com>
2026-07-28 06:20:44 +0300

    create viewsets for cart and cartitems

--- BookStoreApp/views.py
+++ BookStoreApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Book, Category,  Order, OrderItem
-from .serializers import BookSerializer, CategorySerializer , OrderSerializer, OrderItemSerializer
+from .models import Book, Category,  Order, OrderItem , Cart , CartItem
+from .serializers import BookSerializer, CategorySerializer , OrderSerializer, OrderItemSerializer ,CartSerializer, CartItemSerializer
 
 # Create your views here.
 
@@ -13,6 +13,14 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
