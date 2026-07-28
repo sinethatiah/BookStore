f1fb4d99fb5b4e9459c0b075b85ef961a8e7eba9
thatiah <thatiahnia@gmail.com>
2026-07-28 05:58:10 +0300

    make viewsets for order and orderitem models

--- BookStoreApp/views.py
+++ BookStoreApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Book, Category
-from .serializers import BookSerializer, CategorySerializer
+from .models import Book, Category,  Order, OrderItem
+from .serializers import BookSerializer, CategorySerializer , OrderSerializer, OrderItemSerializer
 
 # Create your views here.
 
@@ -12,3 +12,11 @@ class BookViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
