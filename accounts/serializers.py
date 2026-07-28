deff2d5abad7bb347d90d03339eb34a9478072a3
thatiah <thatiahnia@gmail.com>
2026-07-28 09:27:02 +0300

    create a serializer for user registration

---
+++ accounts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'phone_number']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            phone_number=validated_data.get('phone_number', ''),
+        )
+        return user
