ebf7be4847bbf21797cc190c0db3a20d3f83305d
thatiah <thatiahnia@gmail.com>
2026-07-29 08:52:29 +0300

    change the str method in category from self.title to self.name and the one on book from self.name to self.title

--- BookStoreApp/models.py
+++ BookStoreApp/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     def __str__(self):
-        return self.title
+        return self.name
 
 class Book(models.Model):
     STATUS_CHOICES = [
@@ -26,7 +26,7 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.title
     
 class Cart(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
