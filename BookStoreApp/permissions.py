c5aee49abfadebd493d4185dddef48b26bd39064
thatiah <thatiahnia@gmail.com>
2026-07-29 08:48:54 +0300

    add custom IsOwnerOrAdmin class to handle customer

--- BookStoreApp/permissions.py
+++ BookStoreApp/permissions.py
@@ -4,3 +4,8 @@ from rest_framework.permissions import BasePermission
 class IsAdminRole(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'admin'
+class IsOwnerOrAdmin(BasePermission):
+       def has_object_permission(self, request, view, obj):
+        if request.user.role == 'admin':
+            return True
+        return obj.user == request.user
