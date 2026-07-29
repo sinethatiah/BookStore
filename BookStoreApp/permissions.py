df01aff60b1e3af74d8ad672120255a98d4e6aeb
thatiah <thatiahnia@gmail.com>
2026-07-29 10:47:03 +0300

    adjust logic in permission classes as they crash with anonymous users

--- BookStoreApp/permissions.py
+++ BookStoreApp/permissions.py
@@ -3,9 +3,12 @@ from rest_framework.permissions import BasePermission
 
 class IsAdminRole(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'admin'
+        return bool(request.user and request.user.is_authenticated and request.user.role == 'admin')
+
 class IsOwnerOrAdmin(BasePermission):
-       def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
+        if not request.user or not request.user.is_authenticated:
+            return False
         if request.user.role == 'admin':
             return True
         return obj.user == request.user
