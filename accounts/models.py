8c42c992f10401a6f5c8164761cce5ac28416444
thatiah <thatiahnia@gmail.com>
2026-07-29 08:17:31 +0300

    update user model to add roles fot the users

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,9 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('customer', 'Customer'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
     phone_number = models.CharField(max_length=20, blank=True)
