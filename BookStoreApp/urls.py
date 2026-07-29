d44230c729cf8ed6132ceaf87d4ad3a319409363
thatiah <thatiahnia@gmail.com>
2026-07-29 11:52:55 +0300

    add explicit basename to routers for viewsets without static queryset

--- BookStoreApp/urls.py
+++ BookStoreApp/urls.py
@@ -4,11 +4,11 @@ from .views import BookViewSet, CategoryViewSet,OrderViewSet, OrderItemViewSet ,
 router = DefaultRouter()
 router.register('books', BookViewSet)
 router.register('categories', CategoryViewSet)
-router.register('orders', OrderViewSet)
-router.register('order-items', OrderItemViewSet)
-router.register('cart', CartViewSet)
-router.register('cart-items', CartItemViewSet)
-router.register('favorite', FavoriteViewSet)
-router.register('stock-notifications', StockNotificationViewSet)
+router.register('orders', OrderViewSet, basename='order')
+router.register('order-items', OrderItemViewSet, basename='orderitem')
+router.register('cart', CartViewSet, basename='cart')
+router.register('cart-items', CartItemViewSet, basename='cartitem')
+router.register('favorites', FavoriteViewSet, basename='favorite')
+router.register('stock-notifications', StockNotificationViewSet, basename='stocknotification')
 
 urlpatterns = router.urls
