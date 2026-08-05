191771625398e5bc99fcbe981e27a4a38567b928
thatiah <thatiahnia@gmail.com>
2026-08-05 12:13:49 +0300

    chore : finish celery configuration in settings.py

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'BookStoreApp',
     'accounts',
     'rest_framework',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,10 @@ EMAIL_PORT=config("EMAIL_PORT")
 EMAIL_USE_TLS=config("EMAIL_USE_TLS")
 EMAIL_HOST_USER=config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD=config("EMAIL_HOST_PASSWORD")
+
+CELERY_BROKER_URL = config('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
