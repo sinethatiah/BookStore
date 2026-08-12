dd83782041cb593fade728294378fa4f3d6a83d6
sinethatiah <thatiahnia@gmail.com>
2026-08-12 16:34:39 +0300

    chore: add App component with routing setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Routes will be added here */}
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
