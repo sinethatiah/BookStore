ed03616490de82608c40da92cec72fdd20bad1d8
thatiah <thatiahnia@gmail.com>
2026-08-22 15:42:41 +0300

    feat: add auth.css and booklisting.css and remove vanilla style.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BookListing from './pages/BookListing';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Routes will be added here */}
+        <Route path="/" element={<BookListing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
