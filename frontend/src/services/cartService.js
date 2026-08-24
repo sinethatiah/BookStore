3e067c33155ebae6b0f260771fb9568cae95ab04
thatiah <thatiahnia@gmail.com>
2026-08-24 18:01:44 +0300

    feat: add function for updating cart in cart services

--- frontend/src/services/cartService.js
+++ frontend/src/services/cartService.js
@@ -7,6 +7,9 @@ const cartService = {
     api.post('/cart-items/', { book: bookId, quantity }),
 
   removeFromCart: (cartItemId) => api.delete(`/cart-items/${cartItemId}/`),
+
+  updateQuantity: (cartItemId, quantity) =>
+    api.patch(`/cart-items/${cartItemId}/`, { quantity }),
 };
 
 export default cartService;
