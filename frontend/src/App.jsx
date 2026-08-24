0cab53a250c0f5a7eb5f90cd5ab70a2aca7265d1
thatiah <thatiahnia@gmail.com>
2026-08-25 01:32:35 +0300

    chore: update app.jsx to incorporate the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ProtectedRoute, AdminRoute } from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
 import BookListing from './pages/BookListing';
 import BookDetail from './pages/BookDetail';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Cart from './pages/cart';
 import Checkout from './pages/Checkout';
-// import AdminDashboard from './pages/AdminDashboard'; // build later
-
+import './App.css';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<BookListing />} />
           <Route path="/books/:id" element={<BookDetail />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-
           <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
           <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
-
-          {/* <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} /> */}
         </Routes>
       </Router>
     </AuthProvider>
